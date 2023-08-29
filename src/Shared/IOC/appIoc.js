51f4deb50b1a381d7035347ce75afe278b973055
hkfrei <hkfrei@bluewin.ch>
2023-08-29 12:55:43 +0200

    feat: add a ioc config for the app

---
+++ src/Shared/IOC/appIoc.js
@@ -0,0 +1,39 @@
+import Container from "./Container";
+import MapPresenter from "../../Components/Map/MapPresenter";
+import MapRepository from "../../Components/Map/MapRepository";
+import LayerPresenter from "../../Components/Layers/LayerPresenter";
+import FeatureInfoPresenter from "../../Components/FeatureInfo/FeatureInfoPresenter";
+import FeatureInfoRepository from "../../Components/FeatureInfo/FeatureInfoRepository";
+import HttpGateway from "../HttpGateway";
+import UrlGateway from "../UrlGateway";
+import Observable from "../Observable";
+
+const appIoc = new Container();
+
+// pm initial values
+appIoc.register("mapPmInitValue", {});
+appIoc.register("featureInfoPmInitValue", { available: false });
+
+// singletons
+appIoc.singleton("urlGateway", UrlGateway);
+appIoc.singleton("httpGateway", HttpGateway);
+appIoc.singleton("featureInfoRepository", FeatureInfoRepository, [
+  "httpGateway",
+  "featureInfoPm",
+]);
+appIoc.singleton("mapRepository", MapRepository, [
+  "httpGateway",
+  "urlGateway",
+  "mapPm",
+]);
+
+// transients
+appIoc.register("mapPresenter", MapPresenter, ["mapRepository"]);
+appIoc.register("layerPresenter", LayerPresenter, ["mapRepository"]);
+appIoc.register("featureInfoPresenter", FeatureInfoPresenter, [
+  "featureInfoRepository",
+]);
+appIoc.register("mapPm", Observable, ["mapPmInitValue"]);
+appIoc.register("featureInfoPm", Observable, ["featureInfoPmInitValue"]);
+
+export default appIoc;
